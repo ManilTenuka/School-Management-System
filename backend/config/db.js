d5a53cf9c140c990d48d218f87713a8d5994d7e2
ManilTenuka <maniltenuka@gmail.com>
2024-11-01 23:08:43 +0530

    manil

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,11 @@
-require('dotenv').config();
 const mysql = require('mysql2');
 
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_NAME:', process.env.DB_NAME);
-console.log('DB_PORT:', process.env.DB_PORT);
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD ,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    host: 'localhost',          
+    user: 'root',      
+    password: '',   
+    database: 'school_management_system', 
+    port: 3306                   
 });
 
 db.connect((err) => {
